ab0fd3a2e737681c2be128a87ad19250173798fb
Narmada Natarajan <narmadanatarajan3629@gmail.com>
2026-07-14 16:45:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.py
@@ -0,0 +1,30 @@
+class Solution:
+    def majorityElement(self, arr):
+        #code here
+        
+        #Moore Voting Algorithm 
+        
+        n=len(arr)
+        ele=arr[0]
+        cnt=1
+        
+        for i in range(1,n):
+            if(arr[i]==ele):
+                cnt+=1
+            else:
+                cnt-=1
+                
+            if cnt==0:
+                ele=arr[i]
+                cnt=1
+                
+        cnt = 0
+        for x in arr:
+            if x == ele:
+                cnt += 1
+
+        if cnt > n // 2:
+            return ele
+        return -1
+                
+        
